3ad637a131eda36b22f83902ae8f3a82d0c217c8
lana <lana.alfaadhel1@gmail.com>
2024-02-13 22:18:42 +0300

    mm

---
+++ Plantify/Plant.swift
@@ -0,0 +1,18 @@
+//
+//  Plant.swift
+//  Plantify
+//
+//  Created by lana alfaadhel on 13/02/2024.
+//
+
+import SwiftUI
+
+struct Plant: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Plant()
+}
---
+++ Plantify/PlantView.swift
@@ -0,0 +1,18 @@
+//
+//  PlantView.swift
+//  Plantify
+//
+//  Created by lana alfaadhel on 13/02/2024.
+//
+
+import SwiftUI
+
+struct PlantView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PlantView()
+}
