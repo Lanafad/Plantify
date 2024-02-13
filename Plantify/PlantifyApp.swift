669197a39c954fd5d992ef95d3b305dccf7bbf50
lana <lana.alfaadhel1@gmail.com>
2024-02-13 16:46:35 +0300

    Initial Commit

---
+++ Plantify/PlantifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantifyApp.swift
+//  Plantify
+//
+//  Created by lana alfaadhel on 13/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct PlantifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
