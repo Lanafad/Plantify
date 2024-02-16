955356ae8ce1dbb8d49bc77b5a77b23492cb9831
lana <lana.alfaadhel1@gmail.com>
2024-02-16 03:58:29 +0300

    b

---
+++ Plantify/CheckCardView.swift
@@ -0,0 +1,18 @@
+//
+//  CheckCardView.swift
+//  Plantify
+//
+//  Created by lana alfaadhel on 16/02/2024.
+//
+
+import SwiftUI
+
+struct CheckCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CheckCardView()
+}
---
+++ Plantify/CheckCardViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  CheckCardViewModel.swift
+//  Plantify
+//
+//  Created by lana alfaadhel on 16/02/2024.
+//
+
+import SwiftUI
+
+struct CheckCardViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CheckCardViewModel()
+}
