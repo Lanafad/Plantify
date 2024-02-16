f0fa72c2c66df97863101b1a2451f707655419e3
lana <lana.alfaadhel1@gmail.com>
2024-02-17 02:47:08 +0300

    some logic added

---
+++ Plantify/ViewModelsCoordinator.swift
@@ -0,0 +1,18 @@
+//
+//  ViewModelsCoordinator.swift
+//  Plantify
+//
+//  Created by lana alfaadhel on 17/02/2024.
+//
+
+import SwiftUI
+
+struct ViewModelsCoordinator: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ViewModelsCoordinator()
+}
