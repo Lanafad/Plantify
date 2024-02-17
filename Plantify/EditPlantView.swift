c7842a32f75080c39e023b281b3eb2278eecdd47
lana <lana.alfaadhel1@gmail.com>
2024-02-17 21:02:58 +0300

    Most of the functions activated: the seeAll, the check cards and all their functionalities, adding a plant and the card being a navigation link to the plant view, the circle in the plant card changing according to watering progress, the advices, and the tags. All linked with razan’s page

---
+++ Plantify/EditPlantView.swift
@@ -0,0 +1,18 @@
+//
+//  EditPlantView.swift
+//  Plantify
+//
+//  Created by lana alfaadhel on 17/02/2024.
+//
+
+import SwiftUI
+
+struct EditPlantView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditPlantView()
+}
