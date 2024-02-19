4b21448c37c9126c9581fffadaa487d33dbd6860
lana <lana.alfaadhel1@gmail.com>
2024-02-19 17:43:37 +0300

    finallllll app

--- Plantify/ContentView.swift
+++ Plantify/ContentView.swift
@@ -4,17 +4,17 @@
 //
 //  Created by lana alfaadhel on 13/02/2024.
 //
-
-import SwiftUI
-
-struct ContentView: View {
-    var body: some View {
-        SplashView()
-    }
-}
-
-
-
-#Preview {
-    ContentView()
-}
+//
+//import SwiftUI
+//
+//struct ContentView: View {
+//    var body: some View {
+////        SplashView()
+//    }
+//}
+//
+//
+//
+//#Preview {
+//    ContentView()
+//}
